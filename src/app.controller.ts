8811b2e1f27bdd2fc3fc5a0634ae94b71a1fc26a
Sergio Barril <tropped@gmail.com>
2025-03-01 12:35:01 +0100

    Config module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Controller()
 export class AppController {
+  constructor(private configService: ConfigService) {}
+
   @Get("/health")
   getHealth() {
-    return "OK";
+    return `OK: ${this.configService.get<string>("MOCK_ENV_VAR")}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
