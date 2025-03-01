57ce099677e3adf729f4184c09c410b9de57eb5f
Sergio Barril <tropped@gmail.com>
2025-03-01 23:19:43 +0100

    Change drizzle config to allow empty auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
+const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
+
 export default defineConfig({
   schema: "./src/config/database/tables/index.ts",
   out: "./src/config/database/migrations",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    // authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: TURSO_DATABASE_URL!,
+    ...(TURSO_AUTH_TOKEN ? { token: TURSO_AUTH_TOKEN } : {}),
   },
 });
--- src/config/database/drizzle.provider.ts
+++ src/config/database/drizzle.provider.ts
@@ -5,12 +5,14 @@ export const TURSO_DATABASE = "TURSO_DATABASE";
 
 import * as schema from "./tables";
 
+const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
+
 export const DrizzleProvider = {
   provide: TURSO_DATABASE,
   useFactory: () => {
     const client = createClient({
-      url:
-        (process.env.TURSO_DATABASE_URL as string) || "http://127.0.0.1:8080",
+      url: (TURSO_DATABASE_URL as string) || "http://127.0.0.1:8080",
+      ...(TURSO_AUTH_TOKEN ? { authToken: TURSO_AUTH_TOKEN } : {}),
     });
 
     const connection = drizzle(client, { schema });
