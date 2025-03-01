38bcf84f18f21e2b7d73ffe42f42c158b6d9eada
Sergio Barril <tropped@gmail.com>
2025-03-01 17:00:54 +0100

    Implement zod validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
 import { LoggerModule } from "nestjs-pino";
 import getPinoConfig from "./config/loggers/pino.logger";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+import { APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core";
+
 import { LoggingInterceptor } from "./config/interceptors/logging.interceptor";
+import { ZodValidationPipe } from "./config/pipes/zod-validation.pipe";
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { LoggingInterceptor } from "./config/interceptors/logging.interceptor";
     LoggerModule.forRoot(getPinoConfig()),
   ],
   controllers: [AppController],
-  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_PIPE, useClass: ZodValidationPipe },
+  ],
 })
 export class AppModule {}
